401cbd959e7f5e621c1eb26742142f9aa597e1b9
Peter Dodds <peter.dodds@gmail.com>
2015-04-11 21:41:40 +0100

    Fix typo

--- spec/ops_spec.lua
+++ spec/ops_spec.lua
@@ -4,12 +4,12 @@ local ops = require 'fn.ops'
 
 describe("ops", function()
   it("can access op table as a function", function()
-    assert.equal(opts.op["+"], opts.op"+")
+    assert.equal(ops.op["+"], ops.op"+")
   end)
 
   it("can call each operator", function()
-    assert.equal(2+3, opts.op["+"](2,3))
-    assert.equal(2+3, opts.add(2,3))
+    assert.equal(2+3, ops.op["+"](2,3))
+    assert.equal(2+3, ops.add(2,3))
 
     assert.truthy(false) -- todo
   end)
