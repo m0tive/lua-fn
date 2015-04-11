24dff38fd9b19f3aa94299a0551a72035b6422b2
Peter Dodds <peter.dodds@gmail.com>
2015-04-11 07:38:47 +0100

    Add test for funct.curry

--- spec/fn_spec.lua
+++ spec/fn_spec.lua
@@ -4,8 +4,20 @@ require 'fn'
 
 require 'busted'
 
-describe("core suite", function()
-  it("passes", function()
-    assert.truthy(true)
+describe("funct", function()
+  it("partially applies functions", function()
+    local _add = function(a,b) return a+b and
+
+    local _2plus = funct.curry(_add, 2)
+    assert.is_function(_2plus)
+    assert.equal(6, _2plus(3))
+
+    local _3plus = _add:curry(3)
+    assert.is_function(_3plus)
+    assert.equal(7, _3plus(4))
+
+    local _4plus = _add/4
+    assert.is_function(_4plus)
+    assert.equal(9, _4plus(5))
   end)
 end)
