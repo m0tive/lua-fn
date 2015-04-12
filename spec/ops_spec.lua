3d4b36c267242f07941b4e757dff6d87a169881b
Peter Dodds <peter.dodds@gmail.com>
2015-04-12 20:28:07 +0100

    Update ops_spec.lua

--- spec/ops_spec.lua
+++ spec/ops_spec.lua
@@ -39,21 +39,52 @@ describe("ops", function()
     
     -- ops.eq
     assert.equal(2==3, ops.op["=="](2,3))
-    assert.equal(2==3, ops.eq(2==3))
+    assert.equal(2==3, ops.eq(2,3))
 
     -- ops.ne
-    -- ops.lt
-    -- ops.gt
-    -- ops.le
-    -- ops.ge
-    -- ops.concat
-    -- ops.AND
-    -- ops.OR
-    -- ops.NOT
-    -- ops.index
-    -- ops.unm
+    assert.equal(2~=3, ops.op["~="](2,3))
+    assert.equal(2~=3, ops.ne(2,3))
 
-    assert.truthy(false) -- todo
+    -- ops.lt
+    assert.equal(2<3, ops.op["<"](2,3))
+    assert.equal(2<3, ops.le(2,3))
+
+    -- ops.gt
+    assert.equal(2>3, ops.op[">"](2,3))
+    assert.equal(2>3, ops.gt(2,3))
+
+    -- ops.le
+    assert.equal(2<=3, ops.op["<="](2,3))
+    assert.equal(2<=3, ops.le(2,3))
+
+    -- ops.ge
+    assert.equal(2>=3, ops.op[">="](2,3))
+    assert.equal(2>=3, ops.ge(2,3))
+
+    -- ops.concat
+    assert.equal("a".."b", ops.op[".."]("a","b"))
+    assert.equal("a".."b", ops.concat("a","b"))
+
+    -- ops.AND
+    assert.equal(true and 3, ops.op["and"](true, 3))
+    assert.equal(true and 3, ops.AND(true,3))
+
+    -- ops.OR
+    assert.equal(true or 3, ops.op["or"](true,3))
+    assert.equal(true or 3, ops.OR(true,3))
+
+    -- ops.NOT
+    assert.equal(not true, ops.op["not"](true))
+    assert.equal(not true, ops.NOT(true))
+
+    -- ops.index
+    local t = {1,1,2,3,5,8,13}
+    assert.equal(t[3], ops.op["[]"](3))
+    assert.equal(t[3], ops.index(3))
+
+    -- ops.unm
+    assert.equal(-3, ops.op["-"](3))
+    assert.equal(-3, ops.unm(3))
   end)
 end)
     
