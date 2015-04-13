29d305be5f9c9b1aa7b15ef2206b1c6040cc251a
Peter Dodds <peter.dodds@gmail.com>
2015-04-13 22:11:39 +0100

    Add simple fn.seq functions

--- lib/fn/seq.lua
+++ lib/fn/seq.lua
@@ -3,11 +3,18 @@ local funct = require 'fn.funct'
 local seq = {}
 
 -- seq.id
+function seq.id(...) return ... end
+
 -- seq.fst
+function seq.fst(a) return a end
+
 -- seq.snd
+function seq.snd(_,a) return a end
+
 -- seq.nth
--- seq.lst
--- seq.flip
--- seq.rot
+seq.nth = seq.first..select
+
+-- seq.flip (a,b,...) -> (b,a,...)
+-- seq.rot (a,b,c) -> (b,c,a)
 
 return seq
