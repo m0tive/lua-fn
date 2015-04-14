094313d226141914985629f27e79f7f2ee5a86b0
Peter Dodds <peter.dodds@gmail.com>
2015-04-14 13:17:41 +0100

    Add seq.flip and seq.rot

--- lib/fn/seq.lua
+++ lib/fn/seq.lua
@@ -15,6 +15,22 @@ function seq.snd(_,a) return a end
 seq.nth = seq.first..select
 
 -- seq.flip (a,b,...) -> (b,a,...)
+function seq.flip(a,b,...) return b,a,... end
+
 -- seq.rot (a,b,c) -> (b,c,a)
+function fn.rot(a, ...)
+  local n = select('#', ...)
+
+  if n == 0 then
+    return a
+  elseif n == 1 then
+    return unpack(...), a
+  else
+    local args = {...}
+    n = n+1
+    args[n] = a
+    return unpack(args, 1, n)
+  end
+end
 
 return seq
