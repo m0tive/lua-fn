d805195528adb748058fc9ddafb4f82f724b6a7f
Peter Dodds <peter.dodds@gmail.com>
2015-04-15 21:19:01 +0100

    Add trav.elem and trav.map

--- lib/fn/trav.lua
+++ lib/fn/trav.lua
@@ -6,7 +6,24 @@ local fold = require 'fn.fold'
 local trav = {}
 
 -- elem (search)
+function trav.elem(e, t)
+  for _,v in ipairs(t) do
+    if v == e then
+      return true
+    end
+  end
+  return false
+end
+
 -- map
+function trav.map(f, t)
+  local r = {}
+  for k,v in ipairs(t) do
+    r[k] = f(r)
+  end
+  return r
+end
+
 -- sum
 -- product: foldl/op"*"
 
