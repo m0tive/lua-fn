7be83717dacebeef723cb48d6a3ad278df3188da
Peter Dodds <peter.dodds@gmail.com>
2015-04-15 12:57:45 +0100

    Update trav.lua

--- lib/fn/trav.lua
+++ lib/fn/trav.lua
@@ -1,6 +1,13 @@
 -- Methods for traversing arrays
 
+local funct = require 'fn.funct'
+local fold = require 'fn.fold'
+
+local trav = {}
+
 -- elem (search)
 -- map
 -- sum
 -- product: foldl/op"*"
+
+return trav
