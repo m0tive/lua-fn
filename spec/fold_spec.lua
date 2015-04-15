a0effe907793fa58b0890acde9ec656db74aa8a7
Peter Dodds <peter.dodds@gmail.com>
2015-04-15 21:28:28 +0100

    Add fold.foldl1 and foldr1 testd

--- spec/fold_spec.lua
+++ spec/fold_spec.lua
@@ -14,4 +14,21 @@ describe("fold", function()
     assert.equal(8, fold.foldr(_div, 2, {8,12,24,4}))
     assert.equal(3, fold.foldr(_div, 3, {}))
   end)
+
+  it("can fold left over elements with no base", function()
+    local function _add(a,b) return a+b end
+    local function _div(a,b) return a/b end
+    assert.equal(10, fold.foldl1(_add, {1,2,3,4}))
+    assert.equal(1, fold.foldl1(_div, {64,4,2,8}))
+    assert.equal(12, fold.foldl1(_div, {12}))
+  end)
+
+  it("can fold right over elements with no base", function()
+    local function _add(a,b) return a+b end
+    local function _div(a,b) return a/b end
+    assert.equal(10, fold.foldr1(_add, {1,2,3,4}))
+    assert.equal(4, fold.foldr1(_div, {8,12,24,4}))
+    assert.equal(12, fold.foldr1(_div, {12}))
+  end)
+
 end)
