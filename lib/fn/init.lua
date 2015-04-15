4513d95fe1f4ccd0157940bfd945f6e19fb2288e
Peter Dodds <peter.dodds@gmail.com>
2015-04-15 21:02:16 +0100

    Remove folds from fn.init

--- lib/fn/init.lua
+++ lib/fn/init.lua
@@ -97,37 +97,4 @@ end
 --
 fn.negate = fn.curry(fn.composit, fn.ops.NOT)
 
---------------------------------------------------------------------------------
--- Left-fold a binary function over an array of elements using ipairs
---
---     foldl(f,m,[a,b,c]) --> f(f(f(m,a),b),c)
---
-function fn.foldl(m, t, f)
-  local m = m
-  for _, v in ipairs(t) do
-    m = f(m, v)
-  end
-  return m
-end
-
---------------------------------------------------------------------------------
--- Right-fold a binary function over an array of elements using ipairs
---
---     foldr(f,m,[a,b,c]) --> f(a,f(b,f(c,m)))
---
-function fn.foldr(f, m, t)
-  local iter, _, start = ipairs(t)
-
-  function caller(m, i)
-    local i, v = iter(t, i)
-    if v == nil then
-      return m
-    else
-      return f(v, caller(m, i))
-    end
-  end
-
-  return caller(m, start)
-end
-
 return fn
