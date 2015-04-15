116cdfc17720da329e71d48b86204d92bca4fcf7
Peter Dodds <peter.dodds@gmail.com>
2015-04-15 13:29:01 +0100

    Update fold_spec.lua

--- spec/fold_spec.lua
+++ spec/fold_spec.lua
@@ -2,4 +2,16 @@ require 'spec.setup'
 
 local fold = require 'fn.fold'
 
--- TODO
+describe("fold", function()
+  it("can fold left over elements", function()
+    local function _div(a,b) return a/b end
+    assert.equal(2, fold.foldl(_div, 64, {4,2,4}))
+    assert.equal(3, fold.foldl(_div, 3, {}))
+  end)
+  
+  it("can fold right over elements", function()
+    local function _div(a,b) return a/b end
+    assert.equal(8, fold.foldr(_div, 2, {8,12,24,4}))
+    assert.equal(3, fold.foldr(_div, 3, {}))
+  end)
+end)
